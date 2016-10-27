de887feccbf0ffc280781fd774fd9d3b7342cbb3
jizhishushudeyiningbaobao <yn@stu.scu.edu.cn>
2016-10-27 22:14:39 +0800

    机智叔叔的第二版

--- Gibbs/Gibbs2.m
+++ Gibbs/Gibbs2.m
@@ -4,9 +4,9 @@ plot(t,x);
 title('吉布斯现象动画');
 xlabel('t');
 ylabel('x(t)');
-for n=1:2:9
+for n=3:2:99
     pause(0.5);
-    x=x+(4/(n*pi)*sin(n*pi*t));   
+    x=x+(4/(n*pi)*sin(n*t));   
     plot(t,x);
     title('吉布斯现象动画');
     xlabel('t');
