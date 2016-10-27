47190ade3e6621b9b0500b403fcc84f52112d92b
jizhishushudeyiningbaobao <yn@stu.scu.edu.cn>
2016-10-27 22:14:01 +0800

    zz的第一版

--- Gibbs/Gibbs2.m
+++ Gibbs/Gibbs2.m
@@ -4,9 +4,9 @@ plot(t,x);
 title('吉布斯现象动画');
 xlabel('t');
 ylabel('x(t)');
-for n=3:2:99
+for n=1:2:9
     pause(0.5);
-    x=x+(4/(n*pi)*sin(n*t));   
+    x=x+(4/(n*pi)*sin(n*pi*t));   
     plot(t,x);
     title('吉布斯现象动画');
     xlabel('t');
