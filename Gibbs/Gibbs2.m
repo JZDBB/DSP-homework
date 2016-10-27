351714aa876d9cba46175fe5f53aacb16377f2bc
jizhishushudeyiningbaobao <yn@stu.scu.edu.cn>
2016-10-27 22:24:26 +0800

    坐标格子

--- Gibbs/Gibbs2.m
+++ Gibbs/Gibbs2.m
@@ -1,6 +1,7 @@
 t=-2*pi:0.01:2*pi;
 x=4/pi*sin(t);
 plot(t,x);
+grid;
 title('吉布斯现象动画');
 xlabel('t');
 ylabel('x(t)');
@@ -8,6 +9,7 @@ for n=3:2:99
     pause(0.5);
     x=x+(4/(n*pi)*sin(n*t));   
     plot(t,x);
+    grid;
     title('吉布斯现象动画');
     xlabel('t');
     ylabel('x(t)');
