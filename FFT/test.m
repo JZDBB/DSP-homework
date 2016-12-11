4940f97f909e0201f89314a1a6a4059f80e16315
xuyining <1758207513@qq.com>
2016-12-11 20:34:22 +0800

    添加注释

--- FFT/test.m
+++ FFT/test.m
@@ -1,5 +1,11 @@
 a = [1 2 3 4 5 6 7 8];
 n = 1:8;
-A1 = fft(a);
-A2= yn_fft(a);
-A3 = yn_fft1(a);
+tic
+A1 = fft(a);      % result using its own function
+toc
+tic
+A2= yn_fft(a);    % result using iterative algorithm
+toc
+tic
+A3 = yn_fft1(a);  % result using recursive algorithm
+toc               % toc is to estimate the time we take using the function
--- FFT/yn_fft.m
+++ FFT/yn_fft.m
@@ -4,12 +4,12 @@ q = ceil(log2(length(a)));
 A = yn_copy(a,q);
 n = length(a);
 for s = 1:1:log2(n)
-    m = 2^s;
-    Wm = exp(-1i*2*pi/m);
+    m = 2^s;    % s is stand for process time
+    Wm = exp(-1i*2*pi/m); 
     for k = 0:m:n-1
         W = 1;
         for j = 0:1:m/2-1
-            t = W*A(k+j+m/2+1);
+            t = W*A(k+j+m/2+1); 
             u = A(k+j+1);
             A(k+j+1) = u + t;
             A(k+j+m/2+1) = u - t;
@@ -27,11 +27,11 @@ for k = 0:n-1
     b = rev(k,q);
     A(b + 1) = a(k+1);
 end 
-
+% a function which is used to storage processed number
 end
 
 function y = rev(a,q)
-
+% a function which is used to Bit-reversed permutation
 t = dec2bin(a, q);
 t = t(end:-1:1);
 y = bin2dec(t);
