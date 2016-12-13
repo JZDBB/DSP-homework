7a4605cb38c6d8aa5aeb0f94f8b2f0a9ce234181
xuyining <1758207513@qq.com>
2016-12-13 18:52:50 +0800

    加上注释

--- conv/yn_cconv.m
+++ conv/yn_cconv.m
@@ -1,15 +1,15 @@
 function y = yn_cconv(x, h)
-
+% cyclic covolution is used to two sequnence have the same length
 if length(x)~=length(h)
     printf('err');
     return;
 end
-N = length(x);
+N = length(x);% length of the result sequnence
 y = zeros(1,N);
-h = h(end:-1:1)';
-for n = 1:N
+h = h(end:-1:1)';  % sequence fliping and transposing
+for n = 1:N   % convolution sum
     y(n) = x*h;
-    x = circshift(x, [0, 1]);
+    x = circshift(x, [0, 1]);  % shifting
 end
 y = y(end:-1:1);
 end
--- conv/yn_conv.m
+++ conv/yn_conv.m
@@ -1,8 +1,9 @@
 function y = yn_conv(x,h)
+% get the length of the two sequence
 m = length(x);
 n = length(h);
 N = m + n -1;
-if m >= n
+if m >= n   % let the longest to flip
 	t = x;
 	x = h;
 	h = t;
@@ -10,16 +11,16 @@ if m >= n
 	m = n;
 	n = t;
 end
+% pading zeros to make two sequence have the same dimensions
 b = zeros(1,n);
 x = [b x b];
 h = h(end:-1:1);
 b = zeros(1,m + n);
 h = [h b];
-
+% shifting and cycling to attain the convolution sum
 for k = 1:m+n-1
 	h = circshift(h,[0,1]);
 	y(k) = h * x';
-	
 end
 
 end
